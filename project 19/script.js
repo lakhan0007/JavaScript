cb83b2643a383a6f83d27fe2ad27d2ee07814faa
lakhan0007 <lakhansinghrathore2001@gmail.com>
2022-10-19 10:57:20 +0530

    kdfm

--- project 19/script.js
+++ project 19/script.js
@@ -1,3 +1,20 @@
     const tubelight = () =>{
-        let bulbon = document.getElementById('bulbdata');
+        let bulbOn = document.getElementById('bulbdata');
+        let bulbMain = document.getElementById('bulbMain');
+
+        for (let x = 1; x <=6; x++){
+        let bulb = document.getElementById('lightId'.concat(x));
+                    
+                        if(bulb.scroll.match('lightoff')){
+                            bulb.src = "lighton.png";
+                            bulbOn.innerHTML = "Light ON";
+                            bulbOn.style.color = "red";
+                            bulbMain.style.background = "black";
+                        }else{
+                            bulb.src = "lightoff.png";
+                            bulbOn.innerHTML = "Light OFF";
+                            bulbMain.style.Background = "white";
+                            bulbOn.style.color = "black";
+                        }
+     }
     }
